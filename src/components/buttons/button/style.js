08f74b6656f5cf964e4c1171336fd9a79f529806
Peterson <petersonsimiao1@gmail.com>
2021-11-15 23:06:06 -0300

    feat(Component): Criado componente button

---
+++ src/components/buttons/button/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.textColorContrast};
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+  padding: 12px 14px;
+  transition: ${({ theme }) => theme.transition} ease-in-out all;
+  width: 100%;
+
+  &:hover {
+    filter: brightness(0.8);
+  }
+`;
